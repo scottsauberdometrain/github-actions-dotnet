f16e342731792ad4a8962a41c151cc0effc32fe2
scottsauberdometrain <167661955+scottsauberdometrain@users.noreply.github.com>
2024-08-04 13:05:53 -0500

    add app service slots (#30)

--- src/GitHubActionsDotNet.Api/Controllers/WeatherForecastController.cs
+++ src/GitHubActionsDotNet.Api/Controllers/WeatherForecastController.cs
@@ -8,16 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries =
     [
-        "Freezing",
-        "Bracing",
-        "Chilly",
-        "Cool",
-        "Mild",
-        "Warm",
-        "Balmy",
-        "Hot",
-        "Sweltering",
-        "Scorching"
+        "Windy"
     ];
 
     [HttpGet(Name = "GetWeatherForecast")]
